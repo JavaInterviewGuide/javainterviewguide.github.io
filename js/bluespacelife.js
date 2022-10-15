2e84594aff98a318948861e2475efa6ba781d6e5
freelancy <isalex@163.com>
2022-10-15 13:53:09 +0800

    Site updated: 2022-10-15 13:53:09

--- js/bluespacelife.js
+++ js/bluespacelife.js
@@ -64,3 +64,38 @@ function getBrowserVersions() {
     }
     return browser.versions;
 }
+
+/**
+ * 回到主页
+ */
+ function goHome() {
+    window.location.href = '/';
+}
+
+/**
+ * 跳转到文章主页
+ */
+function goToArticles() {
+    window.location.href = '/categories/article';
+}
+
+/**
+ * 跳转到笔记主页
+ */
+function goToNotes() {
+    window.location.href = '/categories/note';
+}
+
+/**
+ * 跳转到小提琴主页
+ */
+function goToViolin() {
+    window.location.href = '/categories/violin';
+}
+
+/**
+ * 跳转到星辰大海主页
+ */
+function goToBeauty() {
+    window.location.href = '/beauty/beauty.html';
+}
--- js/code/code-highlight.js
+++ js/code/code-highlight.js
@@ -3,19 +3,47 @@ $(function () {
     $('pre').wrap('<div class="code-area" style="position: relative"></div>');
 
     // 代码块语言识别
-    var $highlight_lang = $('<div class="code_lang" title="Code"></div>');
+    var $highlight_lang = $('<div class="code_lang" title="Code">Code</div>');
     $('pre').after($highlight_lang);
-    $('pre').each(function () {
-        var code_language = $(this).attr('class');
-
+    $('pre code').each(function () {
+        var code_language = $(this).attr("class");
         if (!code_language) {
             return true;
         };
-        var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
+//        var lang_name = code_language.replace("line-numbers", "").trim().replace("language-", "").trim();
+        var lang_name = code_language;
 
         // 首字母大写
         lang_name = lang_name.slice(0, 1).toUpperCase() + lang_name.slice(1);
-        $(this).siblings(".code_lang").text(lang_name);
+        $(this).parent().siblings(".code_lang").text(lang_name);
+    });
+
+    // 行号
+    $('pre code').each(function () {
+        let lines = $(this).text().split("\n");
+        let rowNum = lines.length;
+//        console.log('行数', rowNum)
+        let size = (rowNum + '').length;
+        let sizeBase = '';
+        for (let i = 0; i < size; i++) {
+            sizeBase = sizeBase + '0';
+        }
+        let newLines = '';
+//        let rlu = $('<ul class="line-numbers-rows"></ul>');
+        for (let i = 1; i <= rowNum; i++) {
+            let num = sizeBase + i;
+            num = num.slice(num.length - size);
+//            let rl = $('<li class="line-numbers">' + num + '</li>');
+            let rl = '<li class="line-numbers">' + num + '</li>';
+//            rlu.append(rl);
+            let lineWithNum = rl + lines[i-1];
+            if (newLines.length > 0) {
+                newLines = newLines + '\n';
+            }
+            newLines = newLines + lineWithNum;
+        }
+        $(this).html(newLines);
+//        $(this).prepend(rlu);
     });
 
     // 代码块一键复制
